6de9029459019541bd526284ecb4f53b90e8660a
purelyaayan <purelyaayan@gmail.com>
2026-04-16 19:05:09 +0530

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,13 +1,11 @@
 function love.conf(t)
     t.identity = nil
 
-    t.window.title = "Aayan's Audio Visualiser cuz Cava won't work on my pc for some reason"
+    t.window.title = "FluxBars"
     t.window.width = 800
     t.window.height = 500
     t.window.resizable = true
     t.window.minwidth = 400
     t.window.minheight = 300
-
-    -- 🔥 THIS replaces the LOVE logo
     t.window.icon = "icon.png"
 end
